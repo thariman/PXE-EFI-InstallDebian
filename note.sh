fb9fb8e437ee3f095913813faf67e06d821ba751
thariman <thariman@gmail.com>
2015-08-08 14:16:24 +0700

    Update note.sh

--- note.sh
+++ note.sh
@@ -17,3 +17,15 @@ sudo "/Applications/VMware Fusion.app/Contents/Library/"vmnet-cfgcli exportconfi
 *.vmx file
 
 $HOME/.docker/machine/machines/dev/dev.vmx
+
+
+try using jpetazzo/pxe
+create a new machine under vmwarefusion
+stop the vm
+add bridge network of my thunderbolt ethernet that is connected to dell xps (eth1)
+run pipework inside boot2docker vm
+sudo ./pipework eth1 sad_feynman 192.168.1.2/24
+able to ping from dell after asigning static ip.  The dhcp doesn't seems to work yet.
+
+next going to try to create boot debian vm and try dhcp directly.
+
